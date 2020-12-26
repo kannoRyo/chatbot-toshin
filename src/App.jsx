13c6b5dcbecdc06ef7835f570ea50a9de3411ddc
kanno <kannoryo@icloud.com>
2020-12-26 19:30:48 +0900

    レイアウトのcssファイル作成

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import './assets/styles/style.css'
+
+function App() {
+  return (
+    <section className="c-section">
+      <div className="c-box">
+        <div style={{height:'480px'}}>
+        <div className="p-chat__row" >
+          <div className="c-chat__bubble">
+            aaaaaaa
+          </div>
+        </div>
+        </div>
+        <div className="c-grid__answer">
+          bbbbbb
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default App;
